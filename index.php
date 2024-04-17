1247c956e07233a365d8ef38ca0957b8a2e745bc
sci <sci@DESKTOP-SFH4UOU>
2024-04-17 11:02:21 +0300

    intial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Task Management</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
